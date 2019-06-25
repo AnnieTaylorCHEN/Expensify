8008e8c84980c54badb7776f3f148591badca797
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-06-25 22:00:16 +0200

    edit server.js back.

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 3000
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
-    res.sendFile('index.html')
+  res.sendFile(path.join(publicPath, 'index.html'))
 })
 
 app.listen(port, () => {
-    console.log('server is up on ' + port)
+  console.log('Server is up! ' + port)
 })
