7d4824935f8ec16834b5a3d9bfd0becc8d9a5bd1
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-06-26 12:56:14 +0200

    set up devDependencies and dist folder.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ module.exports = {
     mode:'production',
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public','dist'),
         filename: 'bundle.js'
     }, 
     plugins: [
@@ -31,6 +31,7 @@ module.exports = {
     'source-map',
     devServer:{
         contentBase: path.join(__dirname, 'public'), 
-        historyApiFallback: true
+        historyApiFallback: true,
+        publicPath: '/dist/'
     }
 }
