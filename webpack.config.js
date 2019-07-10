4fddcd132984c155b11b32a034ef9153d2a1b9d6
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-07-10 18:16:40 +0200

    fix env string error.

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,12 @@ module.exports = {
             filename: "styles.css", 
             chunkFilename: "[id].css"}),
         new webpack.DefinePlugin({
-            'process.env.FIREBASE_API_KEY': JSON.stringify('process.env.FIREBASE_API_KEY'),
-            'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify('process.env.FIREBASE_AUTH_DOMAIN'),
-            'process.env.FIREBASE_DATABASE_URL': JSON.stringify('process.env.FIREBASE_DATABASE_URL'),
-            'process.env.FIREBASE_PROJECT_ID': JSON.stringify('process.env.FIREBASE_PROJECT_ID'),
-            'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify('process.env.FIREBASE_MESSAGING_SENDER_ID'),
-            'process.env.FIREBASE_APP_ID': JSON.stringify('process.env.FIREBASE_APP_ID')
+            'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+            'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+            'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+            'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+            'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+            'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID)
         })
       ],
     module: {
