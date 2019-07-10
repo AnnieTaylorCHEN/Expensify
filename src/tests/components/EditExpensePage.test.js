8546e2422f4a1d4cc26eb4257bf4de14b9048750
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-07-10 21:48:09 +0200

    add startRemoveExpense to remove data from firebase.

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme'
 import { EditExpensePage } from '../../components/EditExpensePage'
 import expenses from '../fixtures/expenses'
 
-let editExpense, removeExpense, history, wrapper
+let editExpense, startRemoveExpense, history, wrapper
 
 beforeEach(() => {
     editExpense = jest.fn()
-    removeExpense = jest.fn()
+    startRemoveExpense = jest.fn()
     history = { push: jest.fn()}
-    wrapper = shallow(<EditExpensePage editExpense={editExpense} removeExpense={removeExpense} history={history} expense={expenses[2]} />)
+    wrapper = shallow(<EditExpensePage editExpense={editExpense} startRemoveExpense={startRemoveExpense} history={history} expense={expenses[2]} />)
 })
 
 test('should render EditExpensePage correctly', () => {
@@ -25,5 +25,5 @@ test('should handle edit expense', () => {
 test('should handle remove expense', () => {
     wrapper.find('button').simulate('click')
     expect(history.push).toHaveBeenLastCalledWith('/')
-    expect(removeExpense).toHaveBeenLastCalledWith({id: expenses[2].id})
+    expect(startRemoveExpense).toHaveBeenLastCalledWith({id: expenses[2].id})
 })
